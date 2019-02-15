ac476f0c4e266af74ac01f920d8b5155f3c2b634
wengsj <497012694@qq.com>
2019-02-15 17:29:39 +0800

    test1

--- src/main/java/com/xino/testgit/TestgitApplication.java
+++ src/main/java/com/xino/testgit/TestgitApplication.java
@@ -5,11 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestgitApplication {
-
+    private  int a;
     public static void main(String[] args) {
         SpringApplication.run(TestgitApplication.class, args
         );
     }
+
 
 }
 
